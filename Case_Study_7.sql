791f7e94e8b8deda86336ab9fd6d304c7137a9ce
impan78 <gowdaimpanhv@gmail.com>
2025-03-04 20:01:39 +0530

    modified files

--- Case_Study_7.sql
+++ Case_Study_7.sql
@@ -4,31 +4,168 @@
 
 -- High Level Sales Analysis
 -- What was the total quantity sold for all products?
+
+SELECT product_name, SUM(qty) total_qty_sold FROM sales S
+JOIN product_details PD
+ON PD.product_id = S.prod_id
+GROUP BY product_name
+ORDER BY product_name;
+
 -- What is the total generated revenue for all products before discounts?
+
+SELECT SUM(qty*price) revenue FROM sales;
+
 -- What was the total discount amount for all products?
+
+SELECT SUM(qty * price * discount / 100) AS total_discount  FROM sales;
+
 -- Transaction Analysis
 -- How many unique transactions were there?
+
+SELECT COUNT(DISTINCT txn_id) trans FROM sales;
+
 -- What is the average unique products purchased in each transaction?
+
+WITH products AS
+(SELECT txn_id, COUNT(DISTINCT prod_id) prod_count FROM sales
+GROUP BY txn_id)
+
+SELECT ROUND(AVG(prod_count)) avg_prod FROM products;
+
 -- What are the 25th, 50th and 75th percentile values for the revenue per transaction?
+
+
+
 -- What is the average discount value per transaction?
+
+WITH txn_discounts AS (
+    SELECT txn_id, SUM(qty *price* discount/100) AS total_discount
+    FROM sales
+    GROUP BY txn_id
+)
+SELECT ROUND(AVG(total_discount), 2) AS avg_discount_per_txn FROM txn_discounts;
+
 -- What is the percentage split of all transactions for members vs non-members?
+
+SELECT 
+	ROUND(100*COUNT(CASE WHEN member = 't' THEN 1 END)/COUNT(*), 2) member_perc_trans,
+    ROUND(100*COUNT(CASE WHEN member = 'f' THEN 1 END)/COUNT(*), 2) non_member_perc_trans
+FROM sales;
+
 -- What is the average revenue for member transactions and non-member transactions?
+
+SELECT 
+	ROUND(AVG(CASE WHEN member = 't' THEN qty*price END), 2) member_avg_revenue,
+    ROUND(AVG(CASE WHEN member = 'f' THEN qty*price END), 2) non_member_avg_revenue
+FROM sales;
+
 -- Product Analysis
 -- What are the top 3 products by total revenue before discount?
+
+SELECT 
+	PD.product_name, SUM(S.qty*S.price) revenue
+FROM sales S
+JOIN product_details PD
+ON S.prod_id = PD.product_id
+GROUP BY product_name
+ORDER BY revenue DESC
+LIMIT 3;
+
 -- What is the total quantity, revenue and discount for each segment?
+
+SELECT PD.segment_name, SUM(S.qty) total_qty, SUM(S.qty*S.price) revenue, 
+SUM(S.qty * S.price * S.discount / 100) AS total_discount FROM sales S
+JOIN product_details PD
+ON S.prod_id = PD.product_id
+GROUP BY PD.segment_name;
+
 -- What is the top selling product for each segment?
+
+WITH products AS
+(SELECT PD.segment_name, PD.product_name, SUM(S.qty) total_qty,
+DENSE_RANK() OVER(PARTITION BY PD.segment_name ORDER BY SUM(S.qty) DESC) rank_
+FROM sales S
+JOIN product_details PD
+ON S.prod_id = PD.product_id
+GROUP BY PD.segment_name, PD.product_name)
+
+SELECT segment_name, product_name, total_qty FROM products
+WHERE rank_ =1;
+
 -- What is the total quantity, revenue and discount for each category?
+
+SELECT PD.category_name, SUM(S.qty) total_qty, SUM(S.qty*S.price) revenue, 
+SUM(S.qty * S.price * S.discount / 100)  total_discount FROM sales S
+JOIN product_details PD
+ON S.prod_id = PD.product_id
+GROUP BY PD.category_name;
+
 -- What is the top selling product for each category?
+
+WITH products AS
+(SELECT PD.category_name, PD.product_name, SUM(S.qty) total_qty,
+DENSE_RANK() OVER(PARTITION BY PD.category_name ORDER BY SUM(S.qty) DESC) rank_
+FROM sales S
+JOIN product_details PD
+ON S.prod_id = PD.product_id
+GROUP BY PD.category_name, PD.product_name)
+
+SELECT category_name, product_name, total_qty FROM products
+WHERE rank_ =1;
+
 -- What is the percentage split of revenue by product for each segment?
+
+SELECT
+	PD.segment_name, PD.product_name,
+    ROUND(100*SUM(S.qty*S.price)/(SELECT SUM(qty*price) FROM sales), 2) percentage
+FROM product_details PD
+JOIN sales S
+ON S.prod_id = PD.product_id
+GROUP BY PD.segment_name, PD.product_name
+ORDER BY PD.segment_name, PD.product_name;
+
+
 -- What is the percentage split of revenue by segment for each category?
+
+SELECT
+	PD.category_name, PD.segment_name,
+    ROUND(100*SUM(S.qty*S.price)/(SELECT SUM(qty*price) FROM sales), 2) percentage
+FROM product_details PD
+JOIN sales S
+ON S.prod_id = PD.product_id
+GROUP BY PD.category_name, PD.segment_name
+ORDER BY percentage DESC;
+
 -- What is the percentage split of total revenue by category?
+
+SELECT
+	PD.category_name,
+    ROUND(100*SUM(S.qty*S.price)/(SELECT SUM(qty*price) FROM sales), 2) percentage
+FROM product_details PD
+JOIN sales S
+ON S.prod_id = PD.product_id
+GROUP BY PD.category_name
+ORDER BY percentage DESC;
+
 -- What is the total transaction “penetration” for each product? (hint: penetration = number of transactions where at least 1 quantity of a product was purchased divided by total number of transactions)
+
+
+
 -- What is the most common combination of at least 1 quantity of any 3 products in a 1 single transaction?
--- Reporting Challenge
--- Write a single SQL script that combines all of the previous questions into a scheduled report that the Balanced Tree team can run at the beginning of each month to calculate the previous month’s values.
 
--- Imagine that the Chief Financial Officer (which is also Danny) has asked for all of these questions at the end of every month.
+WITH concat_products AS
+(SELECT txn_id, GROUP_CONCAT(PD.product_name ORDER BY PD.product_name SEPARATOR ', ') products FROM sales S
+JOIN product_details PD
+ON PD.product_id = S.prod_id
+WHERE S.qty >= 1
+GROUP BY txn_id
+HAVING COUNT(*) = 3)
 
--- He first wants you to generate the data for January only - but then he also wants you to demonstrate that you can easily run the samne analysis for February without many changes (if at all).
+SELECT p1.products FROM concat_products p1
+JOIN concat_products p2
+USING(products)
+GROUP BY p1.products
+HAVING COUNT(*) > 1
+ORDER BY COUNT(*) DESC
+LIMIT 1;
 
--- Feel free to split up your final outputs into as many tables as you need - but be sure to explicitly reference which table outputs relate to which question for full marks :)
--- Case_Study_8.sql
+++ Case_Study_8.sql
@@ -4,18 +4,59 @@
 
 -- Data Exploration and Cleansing
 -- Update the fresh_segments.interest_metrics table by modifying the month_year column to be a date data type with the start of the month
+
+ALTER TABLE interest_metrics 
+ADD COLUMN `date` DATE;
+
+
+UPDATE interest_metrics
+SET `date` = STR_TO_DATE(CONCAT(_year, '-', _month, '-01'), '%Y-%m-%d');
+
+
 -- What is count of records in the fresh_segments.interest_metrics for each month_year value sorted in chronological order (earliest to latest) with the null values appearing first?
+
+SELECT DATE_FORMAT(`date`, '%Y-%m') month_, COUNT(*) records FROM interest_metrics
+GROUP BY month_
+ORDER BY CASE WHEN month_ IS NULL THEN month_ ELSE month_ END;
+
 -- What do you think we should do with these null values in the fresh_segments.interest_metrics
+
+
+
 -- How many interest_id values exist in the fresh_segments.interest_metrics table but not in the fresh_segments.interest_map table? What about the other way around?
+
+WITH CTE AS
+ (SELECT COUNT(*) intrest_id_ims FROM interest_metrics IMS
+ LEFT JOIN interest_map IMP
+ ON IMP.id = IMS.interest_id
+ WHERE IMP.id IS NULL),
+ 
+CTE2 AS
+(SELECT COUNT(*) intrest_id_imp FROM interest_metrics IMS
+ RIGHT JOIN interest_map IMP
+ ON IMP.id = IMS.interest_id
+ WHERE IMS.interest_id IS NULL)
+ 
+SELECT intrest_id_ims, intrest_id_imp FROM CTE, CTE2;
+
 -- Summarise the id values in the fresh_segments.interest_map by its total record count in this table
+
+
 -- What sort of table join should we perform for our analysis and why? Check your logic by checking the rows where interest_id = 21246 in your joined output and include all columns from fresh_segments.interest_metrics and all columns from fresh_segments.interest_map except from the id column.
 -- Are there any records in your joined table where the month_year value is before the created_at value from the fresh_segments.interest_map table? Do you think these values are valid and why?
+
+
 -- Interest Analysis
 -- Which interests have been present in all month_year dates in our dataset?
+
+
+
 -- Using this same total_months measure - calculate the cumulative percentage of all records starting at 14 months - which total_months value passes the 90% cumulative percentage value?
 -- If we were to remove all interest_id values which are lower than the total_months value we found in the previous question - how many total data points would we be removing?
 -- Does this decision make sense to remove these data points from a business perspective? Use an example where there are all 14 months present to a removed interest example for your arguments - think about what it means to have less months present from a segment perspective.
 -- After removing these interests - how many unique interests are there for each month?
+
+
 -- Segment Analysis
 -- Using our filtered dataset by removing the interests with less than 6 months worth of data, which are the top 10 and bottom 10 interests which have the largest composition values in any month_year? Only use the maximum composition value for each interest but you must keep the corresponding month_year
 -- Which 5 interests had the lowest average ranking value?
